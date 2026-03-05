65f8dca3a4bfcd26f218a6260b5e049d967a6477
Ubuntu <ubuntu@ip-172-31-80-29.ec2.internal>
2026-03-05 15:24:50 +0000

    API running with MySQL connector and instrument lookup endpoint

--- db.py
+++ db.py
@@ -1,11 +1,14 @@
+import os
+from urllib.parse import quote_plus
 from sqlalchemy import create_engine
 
-DB_USER = "root"
-DB_PASSWORD = ""
-DB_HOST = "127.0.0.1"
-DB_PORT = 3306
-DB_NAME = "stdata"
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = int(os.getenv("DB_PORT", 3306))
+DB_NAME = os.getenv("DB_NAME")
 
 def get_engine():
-    url = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    password = quote_plus(DB_PASSWORD)
+    url = f"mysql+mysqlconnector://{DB_USER}:{password}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     return create_engine(url, pool_pre_ping=True)
